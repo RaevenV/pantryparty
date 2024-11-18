b6b84ecae14fa7e0527bebf73895e9f091ef0961
samanthacel <samantha.michelle1711@gmail.com>
2024-11-18 23:09:04 +0700

    profile page update

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -18,7 +18,7 @@ const Profile = () => {
   return (
     <>
       <div
-        className=" bg-cream w-full pt-[80px] gap-y-4  min-h-screen flex flex-col justify-start items-center px-16 font-kanit pb-20 relative"
+        className=" bg-cream w-full pt-[80px] gap-y-4  min-h-screen flex flex-col justify-start items-center px-10 font-kanit pb-20 relative"
         style={{
           backgroundImage: `url(${Background})`,
           backgroundSize: "cover",
@@ -27,29 +27,29 @@ const Profile = () => {
         }}
       >
         <Navbar2/>
-        <div className="h-[420px] bg-mainGreen w-full rounded-b-[100px] absolute -top-20"></div>
-        <section className=" flex flex-col justify-start items-center  font-normal z-30  min-h-[200px] w-full rounded-xl relative">
-          <div className="bg-cream shadow-md border-4 border-darkGreen -mt-4 z-40 w-[120px] rounded-[50%] h-[120px] p-2">
+        <div className="h-[450px] bg-mainGreen w-full rounded-b-[100px] absolute -top-20"></div>
+        <section className=" flex flex-col justify-start items-center font-normal z-30  min-h-[200px] w-full rounded-xl relative">
+          <div className="bg-white shadow-md border-4 border-darkGreen -mt-8 z-40 w-[120px] rounded-[50%] h-[120px] p-2">
             <div className=" bg-200 w-full h-full rounded-[50%] "><img src="./onion-profile.png" alt="" /></div>
           </div>
-          <div className="bg-white pt-10 text-[18px] absolute bottom-0 w-full h-36 rounded-xl shadow-xl flex justify-center items-center flex-col">
+          <div className="bg-white pt-10 text-[18px] absolute bottom-[-5px] w-full h-40 rounded-xl shadow-xl flex justify-center items-center flex-col">
             <h2><b className='font-normal text-[17px]'>Chef</b> {userData?.name}</h2>
-            <h2 className="text-[12px] tracking-wide text-slate-500">
+            <h2 className="text-[12px] tracking-wide text-slate-500 mb-2">
               {userData?.age} years old
             </h2>
-            <h2 className="text-[12px] tracking-wide text-slate-500">
-              {userData?.rank}
+            <div className="border-[0.5px] border-darkGreen w-[90%] mb-2 opacity-[70%]"></div>
+            <h2 className="text-[12px] tracking-wide text-slate-500 flex flex-row">
+              <img className="w-5 mr-2" src="./chef-hat.png"/>
+              <div className="text">{userData?.rank}</div>
             </h2>
-            <h2 className="text-[12px] tracking-wide text-slate-500">
-              {userData?.phone}
-            </h2>
-            <h2 className="text-[12px] tracking-wide text-slate-500">
-              {userData?.weeklyCompleted} weeks completed
+            <h2 className="text-[12px] tracking-wide text-slate-500 flex flex-row">
+              <img className="w-5" src="./strikes.png"/>
+              <div className="text">{userData?.weeklyCompleted} recipes this week</div>
             </h2>
           </div>
         </section>
-        <section className="shadow-xl z-30 p-4 font-normal bg-white min-h-[450px] w-full rounded-xl">
-          <div className="text-2xl font-semibold text-darkGreen">Completed Recipes</div>
+        <section className="shadow-xl z-30 p-4 font-normal bg-white min-h-[300px] w-full rounded-xl mt-5">
+          <div className="text-xl font-semibold text-darkGreen">Completed Recipes</div>
           <div className="flex flex-row overflow-x-auto scrollbar scrollbar-thumb-gray-500 scrollbar-track-gray-200 mt-[5px]">
             {userData?.completedRecipes.map((item)=>(
               <div className="mb-[10px] pr-[8px]">
@@ -58,6 +58,7 @@ const Profile = () => {
             ))}
           </div>
         </section>
+
         <div className=" mt-8 flex justify-between items-center w-full px-16">
           <button
             onClick={toggleBack}
